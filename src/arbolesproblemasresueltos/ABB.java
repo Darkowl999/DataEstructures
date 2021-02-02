c4f1b5eb5640b6a687b5efe4012d604183dc2f97
Darkowl999 <raulalbertoortiz290199@hotmail.com>
2021-02-02 14:48:36 -0400

    hago un reboot en el paquete de las clases de ed2 para comprender mejor

--- src/arbolesproblemasresueltos/ABB.java
+++ src/arbolesproblemasresueltos/ABB.java
@@ -1,7 +1,116 @@
 package arbolesproblemasresueltos;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ABB{
-    Nodo raiz;
+    static Nodo raiz; //nodo raiz del arbol binario
+
+    /* Inorder traversal of a binary tree*/
+    static void inorder(Nodo temp) //recorrido inorden
+    {
+        if (temp == null)
+            return;
+
+        inorder(temp.hijoIzquierdo);
+        System.out.print(temp.llave + " ");
+        inorder(temp.hijoDerecho);
+    }
+
+    /*funcion que inserta un elemento en un arbol binario */
+    static void insert(Nodo nodoaInsertar, int llave)
+    {
+        if (nodoaInsertar == null) {
+            raiz = new Nodo(llave);
+            return;
+        }
+        Queue<Nodo> coladeNodos = new LinkedList<Nodo>();
+        coladeNodos.add(nodoaInsertar);
+
+        // Do level order traversal until we find
+        // an empty place.
+        while (!coladeNodos.isEmpty()) {
+            nodoaInsertar = coladeNodos.peek();
+            coladeNodos.remove();
+
+            if (nodoaInsertar.hijoIzquierdo == null) {
+                nodoaInsertar.hijoIzquierdo = new Nodo(llave);
+                break;
+            }
+            else
+                coladeNodos.add(nodoaInsertar.hijoIzquierdo);
+
+            if (nodoaInsertar.hijoDerecho == null) {
+                nodoaInsertar.hijoDerecho = new Nodo(llave);
+                break;
+            }
+            else
+                coladeNodos.add(nodoaInsertar.hijoDerecho);
+        }
+    }
+
+// Función para borrar el nodo más profundo por derecha
+// elemento en árbol binario
+    public void eliminarmasProfundo(Nodo raiz, Nodo nodoaEliminar){
+        Queue<Nodo> coladeNodos= new LinkedList<Nodo>();
+        coladeNodos.add(raiz);
+        Nodo nodoTemporal=null;
+        while (!coladeNodos.isEmpty()){
+            nodoTemporal= coladeNodos.peek();
+            coladeNodos.remove();
+            if (nodoTemporal==nodoaEliminar){
+                nodoTemporal=null;
+                return;
+            }
+            if (nodoTemporal.hijoDerecho!=null){
+                if (nodoTemporal.hijoDerecho==nodoaEliminar){
+                    nodoTemporal.hijoDerecho=null;
+                    return;
+                }
+                else
+                    coladeNodos.add(nodoTemporal.hijoDerecho);
+            }
+            if (nodoTemporal.hijoIzquierdo!=null){
+                if (nodoTemporal.hijoIzquierdo==nodoaEliminar){
+                    nodoTemporal.hijoIzquierdo=null;
+                    return;
+                }
+                else
+                    coladeNodos.add(nodoTemporal.hijoIzquierdo);
+            }
+        }
+    }
+
+// Función para borrar un elemento dado
+// en árbol binario
+    public void eliminar(Nodo raiz, int llave){
+        if (raiz==null)
+            return;
+        if (raiz.hijoIzquierdo==null && raiz.hijoDerecho==null){
+            if (raiz.llave==llave)
+                return;
+            else
+                return;
+        }
+        Queue<Nodo> coladeNodos= new LinkedList<Nodo>();
+        coladeNodos.add(raiz);
+        Nodo nodoTemporal= null, llaveNodo=null;
+        while (!coladeNodos.isEmpty()){
+            nodoTemporal=coladeNodos.peek();
+            coladeNodos.remove();
+            if (nodoTemporal.llave==llave)
+                llaveNodo=nodoTemporal;
+            if (nodoTemporal.hijoIzquierdo!=null)
+                coladeNodos.add(nodoTemporal.hijoIzquierdo);
+        }
+        if (llaveNodo!=null){
+            int x= nodoTemporal.llave;
+            eliminarmasProfundo(raiz,nodoTemporal);
+            llaveNodo.llave=x;
+        }
+
+    }
+
 
 
 
--- src/arbolesproblemasresueltos/main.java
+++ src/arbolesproblemasresueltos/main.java
@@ -5,12 +5,38 @@ import java.util.Stack;
 
 public class main {
     public static void main(String[] args) {
-        Nodo nodo=new Nodo(1);
-        Nodo nodo1=new Nodo(2);
-        Nodo nodo2=new Nodo(3);
-        Nodo nodo3=new Nodo(4);
-        nodo3.setLlave(2);
-        System.out.println(nodo3.getLlave());
+       /* ABB arbol= new ABB();
+        Nodo raiz = new Nodo(10);
+        raiz.hijoIzquierdo = new Nodo(11);
+        raiz.hijoIzquierdo.hijoIzquierdo = new Nodo(7);
+        raiz.hijoDerecho = new Nodo(9);
+        raiz.hijoDerecho.hijoIzquierdo = new Nodo(15);
+        raiz.hijoDerecho.hijoDerecho = new Nodo(8);
+
+        System.out.print("Travesia inorden antes de la insercion"+" => ");
+        arbol.inorder(raiz);
+
+        int key = 12;
+        arbol.insert(raiz,12);
+        System.out.println("\n aca inserto una nueva llave de valor 12 ");
+
+        System.out.print("\n travesia inorden despuesde la insercion"+" => ");
+        arbol.inorder(raiz);*/
+        ABB arbol= new ABB();
+        Nodo raiz=new Nodo(10);
+         raiz.hijoIzquierdo=new Nodo(11);
+         raiz.hijoIzquierdo.hijoIzquierdo=new Nodo(7);
+         raiz.hijoIzquierdo.hijoDerecho=new Nodo(12);
+         raiz.hijoDerecho=new Nodo(9);
+         raiz.hijoDerecho.hijoIzquierdo=new Nodo(15);
+         raiz.hijoDerecho.hijoDerecho=new Nodo(8);
+        System.out.println("travesia en orden antes de la eliminacion");
+        arbol.inorder(raiz);
+        int llave=11;
+        arbol.eliminar(raiz,llave);
+        System.out.println("\n travesia inorden despues de la eliminacion");
+        arbol.inorder(raiz);
 
     }
+
 }
